dda0bd547bcdf54c250fd05f7eb501f674d4c4d4
sdpb21 <jytprf@gmail.com>
2024-07-21 17:56:26 -0300

    00.h changes 3

--- 00.h
+++ 00.h
@@ -5,7 +5,7 @@
 #FUSES NOBROWNOUT               //No brownout reset
 #FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
 
-#use delay(internal=4MHz)
+#use delay(internal=4000000)
 #use STANDARD_IO( A,B )
 
 #use pwm(CCP1,TIMER=2,BITS=10)

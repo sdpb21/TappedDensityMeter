e9d21055518b5b537b66aedfde0d1aff321852af
sdpb21 <jytprf@gmail.com>
2024-07-21 17:53:52 -0300

    00.h changes 1

--- 00.h
+++ 00.h
@@ -1,14 +1,12 @@
 #include <16F628A.h>
 
-#FUSES NOWDT                    //No Watch Dog Timer
-#FUSES NOPUT                    //No Power Up Timer
-#FUSES INTRC_IO                 //Internal RC Osc, no CLKOUT
-#FUSES MCLR                     //Master Clear pin enabled
-#FUSES NOBROWNOUT               //No brownout reset
-#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
-#FUSES NOCPD                    //No EE protection
-#FUSES NOPROTECT                //Code not protected from reading
+#FUSES NOWDT                 	//No Watch Dog Timer
+#FUSES NOMCLR                	//Master Clear pin used for I/O
+#FUSES NOBROWNOUT            	//No brownout reset
+#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
 
-#use delay(int=4000000)
-#USE STANDARD_IO (ALL)
+#use delay(internal=4MHz)
+#use STANDARD_IO( A,B )
+
+#use pwm(CCP1,TIMER=2,FREQUENCY=1000,DUTY=50)
 

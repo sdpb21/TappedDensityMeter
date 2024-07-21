a383bbad1fdc95a08a4e2baeaa5d48dc3a4e1890
sdpb21 <jytprf@gmail.com>
2024-07-21 17:55:09 -0300

    00.h changes 2

--- 00.h
+++ 00.h
@@ -1,12 +1,12 @@
 #include <16F628A.h>
 
-#FUSES NOWDT                 	//No Watch Dog Timer
-#FUSES NOMCLR                	//Master Clear pin used for I/O
-#FUSES NOBROWNOUT            	//No brownout reset
-#FUSES NOLVP                 	//No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
+#FUSES NOWDT                    //No Watch Dog Timer
+#FUSES NOMCLR                   //Master Clear pin used for I/O
+#FUSES NOBROWNOUT               //No brownout reset
+#FUSES NOLVP                    //No low voltage prgming, B3(PIC16) or B5(PIC18) used for I/O
 
 #use delay(internal=4MHz)
 #use STANDARD_IO( A,B )
 
-#use pwm(CCP1,TIMER=2,FREQUENCY=1000,DUTY=50)
+#use pwm(CCP1,TIMER=2,BITS=10)
 
